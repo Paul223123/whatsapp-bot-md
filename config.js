1624219cc62aaf2afb14e6ae8da2540245d02ecf
Paul223123 <148648637+Paul223123@users.noreply.github.com>
2024-06-05 04:29:12 +0100

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ module.exports = {
           logging: false,
         }),
   HANDLERS: (process.env.PREFIX || '^[.,!]').trim(),
-  SUDO: process.env.SUDO || '+2347014691998',
+  SUDO: process.env.SUDO || '',
   HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
   BRANCH: 'master',
